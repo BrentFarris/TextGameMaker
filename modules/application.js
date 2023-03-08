5fe8445e4c12cd072653b52b8ba58ab29bb9afe3
Brent Farris <brentfarris@outlook.com>
2023-03-07 23:29:57 -0600

    Using moved storage location

--- modules/application.js
+++ modules/application.js
@@ -3,6 +3,7 @@ import { Inventory, ItemDatabase } from "./database/item_database.js";
 import { LogDatabase } from "./database/log_database.js";
 import { Media } from "./media.js";
 import { VariableDatabase } from "./database/variable_database.js";
+import { LocalStorage } from "./engine/local_storage.js";
 
 /**
  * @abstract
@@ -26,8 +27,8 @@ export class Application {
 	/** @type {Inventory} */
 	inventory = new Inventory();
 
-	/** @type {Storage} */
-	storage = new Storage();
+	/** @type {LocalStorage} */
+	storage = new LocalStorage();
 
 	/**
 	 * @virtual
